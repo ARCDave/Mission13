88c7ca4962050d28a75b21d630c26338ffc2f259
ARCDave <davidpm.meyers@gmail.com>
2022-03-29 21:44:38 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext _repo { get; set; }
+
+
+        public HomeController(BowlersDbContext temp)
+        {
+            _repo = temp;
+        }
+        
+
+        public IActionResult Index()
+        {
+            var bowlers = _repo.Bowlers.ToList();
+
+            return View(bowlers);
+        }
+
+        
+    }
+}
